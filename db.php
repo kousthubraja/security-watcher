d2135efb416691ca1ba95674543c746c16fcf799
Kousthub Raja <kousthubraja@gmail.com>
2018-02-08 00:39:05 +0530

    added test

--- db.php
+++ db.php
@@ -1,3 +1,3 @@
-<?php	$servername = "mysql";	$username = "user";	$password = "password";	$database = "secdb";
+<?php	$servername = "127.0.0.1";	$username = "user";	$password = "password";	$database = "secdb";
 	// Create connection	$conn = new mysqli($servername, $username, $password, $database);
 	// Check connection	if ($conn->connect_error) {		die("Connection failed: " . $conn->connect_error);	} ?>	
