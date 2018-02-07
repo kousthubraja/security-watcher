2df40cfe43f8c7cb4fafc3569c0a9db75dcbe4af
Kousthub Raja <kousthubraja@gmail.com>
2018-02-08 00:28:22 +0530

    changed db

--- db.php
+++ db.php
@@ -1,3 +1,3 @@
-<?php	$servername = "localhost";	$username = "user";	$password = "password";	$database = "secdb";
+<?php	$servername = "mysql";	$username = "user";	$password = "password";	$database = "secdb";
 	// Create connection	$conn = new mysqli($servername, $username, $password, $database);
 	// Check connection	if ($conn->connect_error) {		die("Connection failed: " . $conn->connect_error);	} ?>	
