216ba6f0e4fa8b30fa843f1f524c355c23d85f64
Kousthub Raja <kousthubraja@gmail.com>
2018-09-03 21:13:09 +0530

    delete file

--- db.php
+++
@@ -1,3 +0,0 @@
-<?php	$servername = "127.0.0.1";	$username = "user";	$password = "password";	$database = "secdb";
-	// Create connection	$conn = new mysqli($servername, $username, $password, $database);
-	// Check connection	if ($conn->connect_error) {		die("Connection failed: " . $conn->connect_error);	} ?>	
